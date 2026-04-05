c2dc47bd00212528d74f13d2c099c1d60a31e301
vrindaaggarwal1307-commits <vrindaaggarwal1307@gmail.com>
2026-04-05 21:52:20 +0530

    experiment 2 js

--- script.js
+++ script.js
@@ -1,48 +1,55 @@
-function calculateResult() {
-    const n=document.getElementById("subjects").value;
-
-    let total=0;
-    let i;
-
-    for(i=0;i<n;i++) 
-    {
-
-        let x=parseFloat(prompt("Enter the subject Number" +(i+1)));
-
-        total=total+x; }
-
-         let average=total/n;
-
-let grade;
-
-if(average >= 90)
-grade = "A";
-
-else if(average >= 75)
-grade = "B";
-
-else if(average >= 60)
-grade = "C";
-
-else if(average >= 50)
-grade = "D";
-
-else
-grade = "Fail";
-
-document.getElementById("result").innerHTML =
-"Average Marks: " + average + "<br>Grade: " + grade;
-
-
-
- let r;
-    if(average>40)
-        r="pass";
-    else r="fail";
-
-    document.getElementById("result").innerHTML += "<br>Result: " + r;
+let employees = []; 
+function addEmployee(){ 
+let name=document.getElementById("name").value; 
+let id=document.getElementById("id").value; 
+let salary=parseInt(document.getElementById("salary").value); 
+let dept=document.getElementById("dept").value; 
+let emp={ 
+name:name, 
+id:id, 
+salary:salary, 
+department:dept 
+}; 
+employees.push(emp); 
+alert("Employee Added"); 
+} 
+function displayEmployees(){ 
+let output=""; 
+for(let emp of employees){ 
+output+=emp.name+" | "+emp.id+" | "+emp.salary+" | "+emp.department+"<br>"; 
+} 
+document.getElementById("result").innerHTML=output; 
+} 
+function filterSalary(){ 
+let filtered=employees.filter(e=>e.salary>50000); 
+let output=""; 
+for(let emp of filtered){ 
+output+=emp.name+" | "+emp.salary+"<br>"; 
+} 
+document.getElementById("result").innerHTML=output; 
+} 
+function totalSalary(){ 
+let total=0; 
+for(let emp of employees){ 
+total+=emp.salary; 
+} 
+document.getElementById("result").innerHTML="Total Salary = "+total; 
+} 
+function averageSalary(){ 
+let total=0; 
+for(let emp of employees){ 
+total+=emp.salary; 
+} 
+let avg=total/employees.length; 
+document.getElementById("result").innerHTML="Average Salary = "+avg; 
+} 
+function countDepartment(){ 
+let dept=prompt("Enter Department Name"); 
+let count=0; 
+for(let emp of employees){ 
+if(emp.department==dept){ 
+count++; 
+} 
+} 
+document.getElementById("result").innerHTML="Employees in "+dept+" ="+count; 
 }
-
-
-//NAME: VRINDA AGGARWAL
-//ROLL NO. : 2503201001337
